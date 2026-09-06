25e3b928b0d3088dbada0876e21dffd5932ca151
Dinis Miguel Ribau Casqueira <48161642+Dinistpn@users.noreply.github.com>
2026-09-06 16:21:40 +0200

    Update 01_data_quality.sql

--- sql/01_data_quality.sql
+++ sql/01_data_quality.sql
@@ -1,2 +1,21 @@
+-- Total rows
 SELECT COUNT(*) AS total_rows
 FROM retail.sales;
+
+
+-- Preview records
+SELECT *
+FROM retail.sales
+LIMIT 10;
+
+
+-- Missing customer IDs
+SELECT COUNT(*) AS missing_customer_ids
+FROM retail.sales
+WHERE customer_id IS NULL;
+
+
+-- Negative quantities
+SELECT COUNT(*) AS negative_quantity_rows
+FROM retail.sales
+WHERE quantity < 0;
