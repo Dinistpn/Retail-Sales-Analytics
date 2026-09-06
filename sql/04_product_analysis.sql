a5287de12eb2841da421f8620b759b5963f62049
Dinis Miguel Ribau Casqueira <48161642+Dinistpn@users.noreply.github.com>
2026-09-06 16:42:18 +0200

    Add product analysis queries for units sold and revenue
    Added SQL queries to analyze units sold and revenue.

--- sql/04_product_analysis.sql
+++ sql/04_product_analysis.sql
@@ -1,3 +1,4 @@
+--units sold
 SELECT
     stock_code,
     description,
@@ -7,4 +8,16 @@ GROUP BY
     stock_code,
     description
 ORDER BY units_sold DESC
+LIMIT 20;
+
+--revenue
+SELECT
+    stock_code,
+    description,
+    SUM(quantity * unit_price) AS revenue
+FROM retail.sales
+GROUP BY
+    stock_code,
+    description
+ORDER BY revenue DESC
 LIMIT 20;
